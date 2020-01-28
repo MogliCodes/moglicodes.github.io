72cb004b5506b9277ade516d086f09157b5ea5d1
DenFin <kontakt@dennisfink.de>
2020-01-28 13:25:20 +0100

    Enhanced scrollspies

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,67 +1,94 @@
 $(document).ready(function(){
 
 
-	/*
-	 * ANIME.JS
-	 */
-	// $(".menuButton").on("click", function(){
-	// 	anime({
-	// 		targets: '.menuButton',
-	// 		left: '0',
-	// 		width: '100%',
-	// 		height: '100%',
-	// 		borderRadius: '0',
-	// 		backgroundColor: '#FFF',
-	// 		easing: 'easeInOutQuad'
-	// 	});
+
+	const menuPanels = document.querySelectorAll(".menu-panel")
+	const scrollspiesList = document.querySelector("#scrollspies ul")
+	const scrollspies = document.querySelectorAll("#scrollspies li")
+
+	menuPanels.forEach(element => {
+		let listItem = document.createElement("li")
+		let anchor = document.createElement("a")
+		let textSpan = document.createElement("span")
+		let circleSpan = document.createElement("span")
+		circleSpan.classList.add("circle")
+		anchor.append(textSpan)
+		anchor.append(circleSpan)
+		textSpan.innerHTML = element.attributes[0].value
+		anchor.href= `#${element.attributes[0].value}`
+		listItem.append(anchor)
+		scrollspiesList.append(listItem)
+	})
+
+	window.addEventListener('scroll', () => {
+		menuPanels.forEach( (panel, index) => {
+
+			if((window.scrollY >= panel.offsetTop || window.scrollY == panel.offsetTop - 1) && window.scrollY <= panel.offsetTop + panel.offsetHeight){
+				panel.classList.add("active")
+				scrollspies[index].classList.add("active")
+			} else {
+				panel.classList.remove("active")
+				scrollspies[index].classList.remove("active")
+			}
+			if(window.scrollY >= panel.offsetTop + panel.offsetHeight || window.scrollY >= panel.offsetTop + panel.offsetHeight){
+				panel.classList.remove("active")
+				scrollspies[index].classList.remove("active")
+			}
+		})
+	})
+
+
+
+	// $(window).on("scroll", function(){
+	// 	var window = $(this);
+	// 	var scrollTop = window.scrollTop();
+	// 	var logo = $(".logo");
+	// 	var animatedOffset = 1 - scrollTop/2
 	// });
 
-	console.log("Hello from app.js")
-	$(window).on("scroll", function(){
-		var window = $(this);
-		var scrollTop = window.scrollTop();
-		var logo = $(".logo");
-		var animatedOffset = 1 - scrollTop/2
-		console.log(scrollTop);
-	});
+
+
+
+
+
 
 	new WOW().init();
 
 
 		// Select all links with hashes
 	$('a[href*="#"]')
-	  // Remove links that don't actually link to anything
-	  .not('[href="#"]')
-	  .not('[href="#0"]')
-	  .click(function(event) {
-	    // On-page links
-	    if (
-	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-	      &&
-	      location.hostname == this.hostname
-	    ) {
-	      // Figure out element to scroll to
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	      // Does a scroll target exist?
-	      if (target.length) {
-	        // Only prevent default if animation is actually gonna happen
-	        event.preventDefault();
-	        $('html, body').animate({
-	          scrollTop: target.offset().top
-	        }, 1000, function() {
-	          // Callback after animation
-	          // Must change focus!
-	          var $target = $(target);
+		  // Remove links that don't actually link to anything
+		  .not('[href="#"]')
+		  .not('[href="#0"]')
+		  .click(function(event) {
+		    // On-page links
+		    if (
+		      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+		      &&
+		      location.hostname == this.hostname
+		    ) {
+		      // Figure out element to scroll to
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		      // Does a scroll target exist?
+		      if (target.length) {
+		        // Only prevent default if animation is actually gonna happen
+		        event.preventDefault();
+		        $('html, body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000, function() {
+		          // Callback after animation
+		          // Must change focus!
+		          var $target = $(target);
 
-	          if ($target.is(":focus")) { // Checking if the target was focused
-	            return false;
-	          } else {
-	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-	          };
-	        });
-	      }
-	    }
+		          if ($target.is(":focus")) { // Checking if the target was focused
+		            return false;
+		          } else {
+		            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+		          };
+		        });
+		      }
+		    }
 	  });
 
 
